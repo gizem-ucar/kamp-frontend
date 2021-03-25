5b1f3a43cf7dcc9990b2a6b99da9d99382ef8209
gizem-ucar <77545358+gizem-ucar@users.noreply.github.com>
2021-03-25 14:02:50 +0300

    17.Ders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //{}=obje demek
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'northwind2';
+  title: string = 'northwind2';
+  user: string = 'Engin Demiroğ';
+  
 }
---
+++ src/app/models/product.ts
@@ -0,0 +1,7 @@
+export interface Product{
+    productId:number;
+    categoryId:number;
+    poductName:string;
+    unitInStock:number;
+    unitPrice:number;
+}
