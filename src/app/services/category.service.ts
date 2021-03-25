19f0369264b1908e53ab7feee4ec9055ae996115
gizem-ucar <77545358+gizem-ucar@users.noreply.github.com>
2021-03-25 23:35:29 +0300

    18.Gün

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Product } from '../models/product';
 import { ListResponseModel } from '../models/listResponseModel';
+import { Category } from '../models/category';
 
 @Injectable({          
   providedIn: 'root'
 })
-export class ProductService {
+export class CategoryService {
 
-  apiUrl = 'https://localhost:44307/api/products/getall';
+  apiUrl = 'https://localhost:44307/api/categories/getall';
 
   constructor(private httpClient: HttpClient) { }
   
-  getProducts():Observable<ListResponseModel<Product>> {
-    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+  getCategories():Observable<ListResponseModel<Category>> {
+    return this.httpClient.get<ListResponseModel<Category>>(this.apiUrl);
   }
 }
